89f7be9721d5a57b18db68643ae16e351623f481
Sékou DIAKITE <sek.diakite@gmail.com>
2024-12-11 20:02:48 +0100

    Cleanups

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#[cfg(target_family = "unix")]
+mod main_unix;
+#[cfg(target_family = "unix")]
+pub use main_unix::*;
+
+#[cfg(target_family = "windows")]
+mod main_windows;
+#[cfg(target_family = "windows")]
+pub use main_windows::*;
