74c6b0366cde76b5ea5677cba847443416b927da
Sékou DIAKITE <sekou.diakite@hpe.com>
2024-12-12 12:18:53 +0100

    Poll

--- src/selector/rawpoll_windows.rs
+++ src/selector/rawpoll_windows.rs
@@ -3,9 +3,53 @@ use std::fmt::Write;
 use windows_sys::Win32::Networking::WinSock::{WSAPoll, WSAPOLLFD};
 pub use windows_sys::Win32::Networking::WinSock::{POLLERR, POLLHUP, POLLIN, POLLOUT, POLLPRI};
 
+use crate::RawSocketFd;
+
 #[repr(transparent)]
 #[derive(Clone, Copy)]
 pub struct PollFD(WSAPOLLFD);
+
+impl PollFD {
+    pub fn new(fd: RawSocketFd, interests: u16) -> Self {
+        Self(WSAPOLLFD {
+            fd: fd.inner as _,
+            events: interests as _,
+            revents: 0 as _,
+        })
+    }
+    pub fn fd(&self) -> RawSocketFd {
+        RawSocketFd::new(self.0.fd as _)
+    }
+    pub fn interests(&self) -> u16 {
+        self.0.events as _
+    }
+    pub fn events(&self) -> u16 {
+        self.0.revents as _
+    }
+    pub fn set_interests(&mut self, interests: u16) {
+        self.0.events = interests as _;
+    }
+    pub fn disable(&mut self) {
+        self.0.fd = RawSocketFd::invalid().inner as _;
+    }
+    pub fn is_disabled(&self) -> bool {
+        !RawSocketFd::new(self.0.fd as _).is_valid()
+    }
+    pub fn enable(&mut self, fd: RawSocketFd, interests: u16) {
+        self.0.fd = fd.inner as _;
+        self.0.events = interests as _;
+        self.0.revents = 0 as _;
+    }
+}
+impl Default for PollFD {
+    fn default() -> Self {
+        Self(WSAPOLLFD {
+            fd: RawSocketFd::invalid().inner as _,
+            events: 0 as _,
+            revents: 0 as _,
+        })
+    }
+}
 
 impl std::fmt::Debug for PollFD {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
--- src/sys/poll.rs
+++ src/sys/poll.rs
@@ -1,7 +1,10 @@
-use rustc_hash::FxHashMap;
+use rustc_hash::{FxHashMap, FxHashSet};
 
 use crate::selector::rawpoll::{sys_poll, PollFD, POLLERR, POLLHUP, POLLIN, POLLOUT, POLLPRI};
+use crate::selector::Interest;
 use crate::RawSocketFd;
+use std::io::{Error, ErrorKind, Result};
+use std::sync::MutexGuard;
 use std::{
     fmt::Debug,
     sync::{Arc, Mutex},
@@ -11,23 +14,174 @@ pub struct Poll {
     inner: Arc<Inner>,
 }
 struct Inner {
-    fds: Mutex<Registration>,
+    waker: crate::sys::Event,
+    /// sys_poll argument
+    registrations: Mutex<Registration>, // Locked first
+    pollfs: Mutex<PollFds>, // Locked second
+}
+
+struct PollFds {
+    active_fds: usize,
+    fds: Vec<PollFD>,
+}
+impl PollFds {
+    fn active_fds(&self) -> usize {
+        self.active_fds
+    }
+    fn len(&self) -> usize {
+        self.fds.len()
+    }
+    fn add_fd(&mut self, fd: RawSocketFd, interests: u16) -> Result<usize> {
+        let mut index = self.active_fds;
+        if self.active_fds == self.fds.len() {
+            self.fds.try_reserve(1)?;
+            self.fds.push(PollFD::new(fd, interests));
+        } else {
+            index = 0;
+            for pfd in self.fds.iter_mut() {
+                if pfd.is_disabled() {
+                    pfd.enable(fd, interests);
+                    break;
+                }
+                index += 1;
+            }
+            assert!(index > 0 && index < self.active_fds);
+        }
+        self.active_fds += 1;
+        Ok(index)
+    }
+    fn mod_fd(&mut self, index: usize, interests: u16) {
+        self.fds[index].set_interests(interests);
+    }
+    fn rem_fd(&mut self, index: usize) {
+        let mut index = index;
+        assert!(index > 0); // Can not remove waker
+        self.active_fds -= 1;
+        if index == self.active_fds {
+            self.fds.remove(index);
+            index -= 1;
+            // Cleanup tail tombstones
+            while index > 0 && self.fds[index].is_disabled() {
+                self.fds.remove(index);
+                index -= 1;
+            }
+        } else {
+            self.fds[index].disable();
+        }
+    }
 }
 
 impl Poll {}
-// struct Inner {
-//     poll_fds: Vec<rawpoll::PollFD>,
-//     tokens: Vec<usize>,
-//     fd_to_index: HashMap<RawSocketFd, u32>,
-//     len: u32,
-// }
+
+impl Inner {
+    fn grab_pollfds_lock_from_mutator(&self) -> Result<MutexGuard<'_, PollFds>> {
+        if let Ok(mut lock) = self.pollfs.try_lock() {
+            Ok(lock)
+        } else {
+            // There is a thread running select, wake it, he will wait for us while trying to grab the registration lock
+            self.waker.notify()?;
+            Ok(self.pollfs.lock().expect("Can not fail"))
+        }
+    }
+    fn register(&self, fd: RawSocketFd, token: usize, interests: u16) -> Result<()> {
+        // Grab the registrations lock
+        let mut registrations = self.registrations.lock().expect("Can not fail");
+
+        // Do not register twice
+        if registrations.entries.contains_key(&fd) {
+            return Err(Error::from(ErrorKind::AlreadyExists));
+        }
+        registrations
+            .entries
+            .try_reserve(1)
+            .or(Err(Error::from(ErrorKind::OutOfMemory)))?;
+
+        // Grab the pollfs lock
+        let mut pollfds = self.grab_pollfds_lock_from_mutator()?;
+        let index = pollfds.add_fd(fd, interests)?;
+        drop(pollfds);
+
+        registrations.entries.insert(
+            fd,
+            FdEntry {
+                index_in_fds: index,
+                token,
+            },
+        );
+        Ok(())
+    }
+    fn reregister(&self, fd: RawSocketFd, token: usize, interests: u16) -> Result<()> {
+        // Grab the registrations lock
+        let mut registrations = self.registrations.lock().expect("Can not fail");
+
+        // Find the entry
+        if let Some(entry) = registrations.entries.get_mut(&fd) {
+            entry.token = token;
+            // Grab the pollfds lock and update
+            self.grab_pollfds_lock_from_mutator()?
+                .mod_fd(entry.index_in_fds, interests);
+            Ok(())
+        } else {
+            Err(Error::from(ErrorKind::NotFound))
+        }
+    }
+    fn unregister(&self, fd: RawSocketFd) -> std::io::Result<()> {
+        // Grab the registrations lock
+        let mut registrations = self.registrations.lock().expect("Can not fail");
+
+        // Find the entry
+        if let Some(entry) = registrations.entries.remove(&fd) {
+            // Grab the pollfds lock and remove
+            self.grab_pollfds_lock_from_mutator()?
+                .rem_fd(entry.index_in_fds);
+            Ok(())
+        } else {
+            Err(Error::from(ErrorKind::NotFound))
+        }
+    }
+    /// Returns `Ok(true)` when the wakup was not done by a registration wake event
+    fn select_inner(
+        &self,
+        events: &mut Vec<crate::selector::Event>,
+        timeout_ms: i32,
+    ) -> Result<bool> {
+        // Grab the registrations lock
+        let registrations = self.registrations.lock().expect("Can not fail");
+        // Grab the pollfs lock
+        let mut pollfds = self.pollfs.lock().expect("Can not fail");
+        // Release the registration lock and wait
+        drop(registrations);
+        let n_events = sys_poll(&mut pollfds.fds, timeout_ms)?;
+        // Remove failed events: TODO:
+        Ok(true)
+    }
+    fn poll(&self, events: &mut Vec<crate::selector::Event>) -> Result<usize> {
+        events.clear();
+        while events.len() == 0 && !self.select_inner(events, 0)? {}
+        Ok(events.len())
+    }
+    fn wait(&self, events: &mut Vec<crate::selector::Event>, timeout_ms: i32) -> Result<usize> {
+        debug_assert!(timeout_ms != 0);
+        let timeout_ms = if timeout_ms > 0 { timeout_ms } else { i32::MAX };
+        let deadline =
+            std::time::Instant::now() + std::time::Duration::from_millis(timeout_ms as _);
+        events.clear();
+        while events.len() == 0 && timeout_ms > 0 {
+            self.select_inner(events, timeout_ms)?;
+            if events.len() == 0 && std::time::Instant::now() >= deadline {
+                return Ok(0usize);
+            }
+        }
+        Ok(events.len())
+    }
+}
 
 #[derive(Debug, Clone)]
 struct Registration {
-    /// sys_poll argument
-    fds: Vec<PollFD>,
     /// maps an fd to its index in fds and it's associated token
-    data: FxHashMap<RawSocketFd, FdEntry>,
+    entries: FxHashMap<RawSocketFd, FdEntry>,
+    /// fds beeing removed before the next "poll()"
+    pending_removal: FxHashSet<RawSocketFd>,
 }
 
 #[derive(Debug, Copy, Clone)]
