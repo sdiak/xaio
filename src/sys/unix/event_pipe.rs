bf8e5c9960b307a054bdd346af5b7c42d00b38e8
Sékou DIAKITE <sek.diakite@gmail.com>
2024-12-14 09:14:07 +0100

    Event(unix:pipe) => OwnedFd

--- src/sys/unix/event_pipe.rs
+++ src/sys/unix/event_pipe.rs
@@ -1,7 +1,10 @@
-use crate::{libc_close_log_on_error, libc_pipe2, libc_read, libc_write_all, selector::rawpoll};
+use crate::{
+    libc_close_log_on_error, libc_pipe2, libc_read, libc_write_all, selector::rawpoll, RawSocketFd,
+};
 
 use std::{
     io::{Error, ErrorKind, Result},
+    os::fd::{AsRawFd, FromRawFd, OwnedFd},
     sync::Arc,
 };
 
@@ -13,17 +16,22 @@ use std::{
 pub struct Event {
     handle: Arc<Inner>,
 }
-#[repr(C)]
-#[derive(Debug, Clone)]
-struct Inner {
-    read_end: libc::c_int,
-    write_end: libc::c_int,
-}
-impl Drop for Inner {
-    fn drop(&mut self) {
-        libc_close_log_on_error(self.read_end);
-        libc_close_log_on_error(self.write_end);
+impl PartialEq for Event {
+    fn eq(&self, other: &Self) -> bool {
+        self.handle.read_end.as_raw_fd() == other.handle.read_end.as_raw_fd()
     }
+}
+impl Eq for Event {}
+impl std::hash::Hash for Event {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.handle.read_end.as_raw_fd().hash(state);
+    }
+}
+#[repr(C)]
+#[derive(Debug)]
+struct Inner {
+    read_end: OwnedFd,
+    write_end: OwnedFd,
 }
 
 impl Clone for Event {
@@ -39,31 +47,27 @@ impl Event {
         let (read_end, write_end) = libc_pipe2(true, true)?;
         Ok(Self {
             handle: Arc::new(Inner {
-                read_end,
-                write_end,
+                read_end: unsafe { OwnedFd::from_raw_fd(read_end) },
+                write_end: unsafe { OwnedFd::from_raw_fd(write_end) },
             }),
         })
     }
     /// Notify a waiter (multiple notification may be coalesced into one)
     pub fn notify(&self) -> Result<()> {
         let unpark_msg = 1u8.to_ne_bytes();
-        libc_write_all((*self.handle).write_end, &unpark_msg, true)
+        libc_write_all((*self.handle).write_end.as_raw_fd(), &unpark_msg, true)
     }
 
     #[inline]
     pub(crate) unsafe fn get_native_handle(&self) -> libc::c_int {
-        (*self.handle).read_end
+        (*self.handle).read_end.as_raw_fd()
     }
 
     /// Waits for the event to be notified or for `timeout_ms` milliseconds
     pub fn wait(&self, timeout_ms: i32) -> Result<()> {
-        let fd = (*self.handle).read_end;
+        let fd = (*self.handle).read_end.as_raw_fd();
         let mut buffer = [0u8; 256];
-        let pollfd = &mut [rawpoll::PollFD {
-            fd: fd,
-            events: rawpoll::POLLIN,
-            revents: 0 as _,
-        }];
+        let pollfd = &mut [rawpoll::PollFD::new(RawSocketFd::new(fd), rawpoll::POLLIN)];
         loop {
             match libc_read(fd, &mut buffer, false) {
                 Ok(len) => {
