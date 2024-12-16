461b9d9ebd74256a6e2edd886f3d61e2e2c580e2
Sékou DIAKITE <sek.diakite@gmail.com>
2024-12-16 17:14:21 +0100

    Formating

--- src/sys/unix/driver_linux.rs
+++ src/sys/unix/driver_linux.rs
@@ -1,14 +1,12 @@
+use super::{EPoll, Event};
+use num;
+use std::io::{Error, Result};
 use std::mem::MaybeUninit;
 use std::ptr::NonNull;
 use std::sync::LazyLock;
-use std::io::{Error, Result};
-use super::{EPoll, Event};
 use uring_sys2;
-use num;
-
 
 pub static PROBE: LazyLock<Probe> = LazyLock::new(Probe::new);
-
 
 #[derive(Debug)]
 pub(crate) struct Driver {
@@ -17,7 +15,6 @@ pub(crate) struct Driver {
     waker: Event, // TODO: register
     config: crate::capi::xconfig_s,
 }
-
 
 impl Driver {
     pub(crate) fn new(config_hints: &crate::capi::xconfig_s) -> Result<Self> {
@@ -28,7 +25,11 @@ impl Driver {
         } else {
             config.completion_queue_depth = num::clamp(config.completion_queue_depth, 16, 4096);
         }
-        config.flags = config.flags & (crate::capi::XCONFIG_FLAG_FAST_POLL | crate::capi::XCONFIG_FLAG_CLOSE_ON_EXEC | crate::capi::XCONFIG_FLAG_ATTACH_SINGLE_ISSUER | crate::capi::XCONFIG_FLAG_CLOSE_ON_EXEC );
+        config.flags = config.flags
+            & (crate::capi::XCONFIG_FLAG_FAST_POLL
+                | crate::capi::XCONFIG_FLAG_CLOSE_ON_EXEC
+                | crate::capi::XCONFIG_FLAG_ATTACH_SINGLE_ISSUER
+                | crate::capi::XCONFIG_FLAG_CLOSE_ON_EXEC);
         let waker = Event::new()?;
         let mut ring = URing::invalid();
         let probe = &*PROBE;
@@ -41,10 +42,14 @@ impl Driver {
         } else {
             epoll = EPoll::new((config.flags & crate::capi::XCONFIG_FLAG_CLOSE_ON_EXEC) != 0)?;
         }
-        Ok(Self { ring, epoll, waker, config})
+        Ok(Self {
+            ring,
+            epoll,
+            waker,
+            config,
+        })
     }
 }
-
 
 #[derive(Debug)]
 struct URing {
@@ -62,22 +67,28 @@ impl URing {
     fn invalid() -> Self {
         let mut ring = unsafe { MaybeUninit::<uring_sys2::io_uring>::zeroed().assume_init() };
         ring.ring_fd = -1;
-        Self { ring, features: 0u32 }
+        Self {
+            ring,
+            features: 0u32,
+        }
     }
     fn new(config: &mut crate::capi::xconfig_s, probe: &Probe) -> Result<Self> {
         // TODO:  SINGLE ISSUER TASKRUN, ...
         // https://manpages.debian.org/unstable/liburing-dev/io_uring_setup.2.en.html
         let mut ring = unsafe { MaybeUninit::<uring_sys2::io_uring>::zeroed().assume_init() };
-        let mut params = unsafe { MaybeUninit::<uring_sys2::io_uring_params>::zeroed().assume_init() };
+        let mut params =
+            unsafe { MaybeUninit::<uring_sys2::io_uring_params>::zeroed().assume_init() };
         params.flags = uring_sys2::IORING_SETUP_CQSIZE;
-        if (config.flags & crate::capi::XCONFIG_FLAG_ATTACH_HANDLE) != 0  && probe.kernel.has_attach_wq() {
+        if (config.flags & crate::capi::XCONFIG_FLAG_ATTACH_HANDLE) != 0
+            && probe.kernel.has_attach_wq()
+        {
             params.wq_fd = config.attach_handle as _;
             params.flags |= uring_sys2::IORING_SETUP_ATTACH_WQ;
         } else {
             config.flags &= !crate::capi::XCONFIG_FLAG_ATTACH_HANDLE;
             params.wq_fd = -1i32 as _;
         }
-        if config.kernel_poll_timeout_ms > 0 && probe.kernel.has_sq_thread_idle()  {
+        if config.kernel_poll_timeout_ms > 0 && probe.kernel.has_sq_thread_idle() {
             params.sq_thread_idle = config.kernel_poll_timeout_ms;
             params.flags |= uring_sys2::IORING_SETUP_SQPOLL;
         } else {
@@ -86,9 +97,18 @@ impl URing {
         if probe.kernel.has_setup_submit_all() {
             params.flags |= uring_sys2::IORING_SETUP_SUBMIT_ALL;
         }
-        let status = unsafe { uring_sys2::io_uring_queue_init_params(params.sq_entries, &mut ring as _, &mut params as _) };
+        let status = unsafe {
+            uring_sys2::io_uring_queue_init_params(
+                params.sq_entries,
+                &mut ring as _,
+                &mut params as _,
+            )
+        };
         if status >= 0 {
-            Ok(Self{ring, features: params.features})
+            Ok(Self {
+                ring,
+                features: params.features,
+            })
         } else {
             Err(Error::from_raw_os_error(-status))
         }
@@ -104,7 +124,14 @@ struct Sqe(*mut uring_sys2::io_uring_sqe);
 
 impl Sqe {
     #[inline(always)]
-    fn prep_read_write(&mut self, op: libc::c_int, fd: libc::c_int, addr: *mut libc::c_void, len: u32, offset: u64) {
+    fn prep_read_write(
+        &mut self,
+        op: libc::c_int,
+        fd: libc::c_int,
+        addr: *mut libc::c_void,
+        len: u32,
+        offset: u64,
+    ) {
         let sqe = unsafe { &mut *self.0 };
         sqe.opcode = op as _;
         sqe.fd = fd;
@@ -119,11 +146,10 @@ impl Sqe {
     }
 }
 
-
 #[derive(Debug)]
 pub struct Probe {
     is_supported: [bool; OP_FACE.len()],
-    kernel: KernelVersion
+    kernel: KernelVersion,
 }
 impl Probe {
     fn has_io_uring(&self) -> bool {
@@ -139,42 +165,51 @@ struct KernelVersion {
 impl KernelVersion {
     fn from_cstr(cstr: *const libc::c_char) -> Self {
         if cstr.is_null() {
-            return Self{  major: -1, minor: 0 };
+            return Self {
+                major: -1,
+                minor: 0,
+            };
         }
         let mut offset = 0isize;
         let mut major = 0i32;
         let mut minor = 0i32;
-        let mut c = unsafe { *cstr.offset(offset) as u8 as char};
+        let mut c = unsafe { *cstr.offset(offset) as u8 as char };
         while '0' <= c && c <= '9' {
             major = (major * 10) + (c as i32 - '0' as i32);
             offset += 1;
-            c = unsafe { *cstr.offset(offset) as u8 as char};
+            c = unsafe { *cstr.offset(offset) as u8 as char };
         }
         if c == '.' {
             offset += 1;
-            c = unsafe { *cstr.offset(offset) as u8 as char};
+            c = unsafe { *cstr.offset(offset) as u8 as char };
             while '0' <= c && c <= '9' {
                 minor = (minor * 10) + (c as i32 - '0' as i32);
                 offset += 1;
-                c = unsafe { *cstr.offset(offset) as u8 as char};
+                c = unsafe { *cstr.offset(offset) as u8 as char };
             }
         }
         if major > 0 && minor >= 0 {
             Self { major, minor }
         } else {
-            Self{  major: -1, minor: 0 }
+            Self {
+                major: -1,
+                minor: 0,
+            }
         }
     }
     fn new() -> Self {
-        let mut uname  = unsafe { MaybeUninit::<libc::utsname>::zeroed().assume_init() };
+        let mut uname = unsafe { MaybeUninit::<libc::utsname>::zeroed().assume_init() };
         if (unsafe { libc::uname(&mut uname as *mut libc::utsname) } < 0) {
-            log::error!("libc::uname(*mut libc::utsname) failed: {}", Error::last_os_error());
+            log::error!(
+                "libc::uname(*mut libc::utsname) failed: {}",
+                Error::last_os_error()
+            );
             Self { major: 0, minor: 0 }
         } else {
             Self::from_cstr(uname.release.as_ptr())
         }
     }
-    fn ge(&self, major: i32, minor: i32)-> bool  {
+    fn ge(&self, major: i32, minor: i32) -> bool {
         self.major > major || (self.major == major && self.minor >= minor)
     }
     fn has_io_uring(&self) -> bool {
@@ -184,7 +219,7 @@ impl KernelVersion {
     fn has_registered_buffers(&self) -> bool {
         self.ge(5, 19)
     }
-    fn has_msg_ring(&self)  -> bool {
+    fn has_msg_ring(&self) -> bool {
         self.major >= 6
     }
     fn has_recv_multishot(&self) -> bool {
@@ -220,30 +255,35 @@ impl Probe {
     pub fn unsuported() -> Self {
         Self {
             is_supported: [false; OP_FACE.len()],
-            kernel: KernelVersion { major: -1, minor: 0 }
+            kernel: KernelVersion {
+                major: -1,
+                minor: 0,
+            },
         }
     }
     pub fn new() -> Self {
-        let mut is_supported  = [false; OP_FACE.len()];
-        
+        let mut is_supported = [false; OP_FACE.len()];
+
         let kernel = KernelVersion::new();
         if !kernel.has_io_uring() {
             return Self::unsuported();
         }
-        let probe = unsafe {
-            uring_sys2::io_uring_get_probe()
-        };
+        let probe = unsafe { uring_sys2::io_uring_get_probe() };
         if probe.is_null() {
             return Self::unsuported();
         }
         for op in 0..OP_FACE.len() {
-            is_supported[op] = unsafe { uring_sys2::io_uring_opcode_supported(probe, op as _) } != 0;
-            // if unsafe { uring_sys2::io_uring_opcode_supported(probe, op as _) } != 0 {
-            //     println!("{op}: {} : true", OP_FACE[op]);
-            // }
+            is_supported[op] =
+                unsafe { uring_sys2::io_uring_opcode_supported(probe, op as _) } != 0;
+            if unsafe { uring_sys2::io_uring_opcode_supported(probe, op as _) } != 0 {
+                println!("{op}: {} : true", OP_FACE[op]);
+            }
         }
         unsafe { uring_sys2::io_uring_free_probe(probe) };
-        Self{is_supported, kernel}
+        Self {
+            is_supported,
+            kernel,
+        }
     }
 }
 
@@ -297,13 +337,13 @@ const OP_FACE: &'static [&'static str] = &[
     "IORING_OP_URING_CMD",
     "IORING_OP_SEND_ZC",
     "IORING_OP_SENDMSG_ZC",
-	"IORING_OP_READ_MULTISHOT",
-	"IORING_OP_WAITID",
-	"IORING_OP_FUTEX_WAIT",
-	"IORING_OP_FUTEX_WAKE",
-	"IORING_OP_FUTEX_WAITV",
-	"IORING_OP_FIXED_FD_INSTALL",
+    "IORING_OP_READ_MULTISHOT",
+    "IORING_OP_WAITID",
+    "IORING_OP_FUTEX_WAIT",
+    "IORING_OP_FUTEX_WAKE",
+    "IORING_OP_FUTEX_WAITV",
+    "IORING_OP_FIXED_FD_INSTALL",
     "IORING_OP_FTRUNCATE",
     "IORING_OP_BIND",
-	"IORING_OP_LISTEN",
+    "IORING_OP_LISTEN",
 ];
