62d7e6ff37f7ddc3d0a56b219c054cf671fca912
Sékou DIAKITE <sek.diakite@gmail.com>
2024-12-17 20:14:41 +0100

    sys::ThreadId

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -18,6 +18,8 @@ impl std::fmt::Debug for EventCallBack {
 
 #[cfg(target_family = "unix")]
 mod unix;
+use std::{fmt::Debug, usize};
+
 #[cfg(target_family = "unix")]
 pub use unix::*;
 
@@ -28,28 +30,77 @@ pub use windows::*;
 
 pub mod poll;
 
-#[cfg(all(
-    target_arch = "x86_64",
-    any(target_os = "linux", target_os = "freebsd")
-))]
-#[inline(always)]
-pub fn get_current_thread_id() -> usize {
-    use std::arch::asm;
-    let mut id: usize;
-    unsafe {
-        asm!(
-            "mov {id}, fs:0",
-            id = lateout(reg) id,
-            options(nostack, pure, readonly),
-        );
+#[repr(C)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ThreadId(usize);
+impl Debug for ThreadId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.is_invalid() {
+            f.write_str("ThreadId()")
+        } else {
+            f.write_fmt(format_args!(
+                "ThreadId({:?})",
+                self.0 as *const libc::c_void
+            ))
+        }
     }
-    id as _
 }
-#[cfg(not(all(
-    target_arch = "x86_64",
-    any(target_os = "linux", target_os = "freebsd")
-)))]
-#[inline(always)]
-pub fn get_current_thread_id() -> std::thread::ThreadId {
-    std::thread::current().id()
+impl ThreadId {
+    #[inline(always)]
+    pub fn current() -> Self {
+        Self(__get_current_thread_id())
+    }
+    #[inline(always)]
+    pub fn invalid() -> Self {
+        Self(usize::MAX)
+    }
+    #[inline(always)]
+    pub fn is_invalid(&self) -> bool {
+        self.0 == usize::MAX
+    }
+    #[inline(always)]
+    pub fn as_usize(&self) -> usize {
+        self.0
+    }
+}
+
+cfg_if::cfg_if! {
+    if #[cfg(all(
+        target_arch = "x86_64",
+        any(target_os = "linux", target_os = "freebsd")
+    ))] {
+        #[inline(always)]
+        pub fn __get_current_thread_id() -> usize {
+            use std::arch::asm;
+            let mut id: usize;
+            unsafe {
+                asm!(
+                    "mov {id}, fs:0",
+                    id = lateout(reg) id,
+                    options(nostack, pure, readonly),
+                );
+            }
+            id as _
+
+        }
+    } else {
+        #[inline(always)]
+        fn __get_current_thread_id() -> usize {
+            use std::cell::Cell;
+            thread_local!(static ID: Cell<usize> = const { Cell::new(usize::MAX) });
+            let mut id = ID.get();
+            if id != usize::MAX {
+                id
+            } else {
+                id = __next_thread_id();
+                ID.set(id);
+                id
+            }
+        }
+        #[inline(never)]
+        fn __next_thread_id() -> usize {
+            static NEXT: std::sync::atomic::AtomicUsize  = std::sync::atomic::AtomicUsize::new(0usize);
+            NEXT.fetch_add(1, std::sync::atomic::Ordering::Relaxed)
+        }
+    }
 }
