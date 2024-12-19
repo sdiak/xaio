c74947646995ed46f5f14bf6238a43b7d7fa6eb3
Sékou DIAKITE <sekou.diakite@hpe.com>
2024-12-19 17:47:06 +0100

    Prep statx

--- xaio-core/src/sys/windows/statx.rs
+++ xaio-core/src/sys/windows/statx.rs
@@ -2,6 +2,14 @@ use crate::sys::StatX;
 
 impl StatX {
     pub fn do_something_else() -> i32 {
+        // let mut opts = OpenOptions::new();
+        // //
+        // opts.access_mode(0);
+        // opts.custom_flags(c::FILE_FLAG_BACKUP_SEMANTICS | reparse.as_flag());
+        // std::fs::metadata
         43
     }
 }
+
+// GetFileInformationByHandleEx
+//
