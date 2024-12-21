a139b2a4412938cf7eedbcb67a84988a897a3bb5
Sékou DIAKITE <sek.diakite@gmail.com>
2024-12-21 16:34:37 +0100

    Various queues

--- xaio-core/src/collection/slist.rs
+++ xaio-core/src/collection/slist.rs
@@ -1,15 +1,16 @@
-use std::{marker::PhantomData, pin::Pin};
+use std::{marker::PhantomData, sync::atomic::Ordering};
 
-use super::{SListLink, SListNode};
+use super::{NodeRef, SLink, SListNode};
 
 pub struct SList<T: SListNode> {
-    pub(crate) head: *mut SListLink,
-    pub(crate) tail: *mut SListLink,
+    pub(crate) head: *mut SLink,
+    pub(crate) tail: *mut SLink,
     _phantom: PhantomData<T>,
 }
 
 impl<T: SListNode> SList<T> {
-    pub fn new() -> SList<T> {
+    /// Returns a new empty list
+    pub const fn new() -> SList<T> {
         Self {
             head: std::ptr::null_mut(),
             tail: std::ptr::null_mut(),
@@ -17,5 +18,239 @@ impl<T: SListNode> SList<T> {
         }
     }
 
-    pub fn push_back(&mut self, node: Pin<&mut T>) {}
+    #[inline(always)]
+    /// Returns `true` if the list is empty
+    pub fn is_empty(&self) -> bool {
+        self.head.is_null()
+    }
+
+    #[inline(always)]
+    /// # Returns
+    ///  * `Some(front)` the front of the list when `!self.is_empty()`
+    ///  * `None` when `self.is_empty()`
+    pub fn front<'a>(&'a self) -> Option<NodeRef<'a, Self, T>> {
+        SLink::into_ref::<Self, T>(self, self.head)
+    }
+
+    #[inline(always)]
+    /// # Returns
+    ///  * `Some(back)` the back of the list when `!self.is_empty()`
+    ///  * `None` when `self.is_empty()`
+    pub fn back<'a>(&'a self) -> Option<NodeRef<'a, Self, T>> {
+        SLink::into_ref::<Self, T>(self, self.tail)
+    }
+
+    /// Adds a node at the front of the list
+    ///
+    /// # Arguments
+    ///  * `new_front` the new front of the list
+    /// # Complexity
+    ///  * O(1)
+    pub fn push_front(&mut self, new_front: Box<T>) {
+        let new_head: *mut SLink = SLink::from::<T>(new_front);
+        unsafe { (*new_head).list_set_next(self.head, Ordering::Relaxed) };
+        if self.head.is_null() {
+            self.tail = new_head;
+        }
+        self.head = new_head;
+    }
+
+    /// Removes and returns the front of the list when `!self.is_empty()`
+    ///
+    /// # Returns
+    ///  * `Some(old_front)` the old front of the list when `!self.is_empty()`
+    ///  * `None` when `self.is_empty()`
+    /// # Complexity
+    ///  * O(1)
+    pub fn pop_front(&mut self) -> Option<Box<T>> {
+        let old_head = self.head;
+        if !old_head.is_null() {
+            self.head = unsafe { (*old_head).list_pop_next(Ordering::Relaxed) };
+            if self.head.is_null() {
+                self.tail = std::ptr::null_mut();
+            }
+            Some(SLink::into::<T>(old_head))
+        } else {
+            None
+        }
+    }
+
+    /// Adds a node at the back of the list
+    ///
+    /// # Arguments
+    ///  * `new_back` the new back of the list
+    /// # Complexity
+    ///  * O(1)
+    pub fn push_back(&mut self, new_back: Box<T>) {
+        let new_tail = SLink::from::<T>(new_back);
+        unsafe { (*new_tail).list_set_next(std::ptr::null_mut(), Ordering::Relaxed) };
+        if self.tail.is_null() {
+            self.head = new_tail;
+        } else {
+            unsafe { (*self.tail).list_update_next(new_tail, Ordering::Relaxed) };
+        }
+        self.tail = new_tail;
+    }
+
+    /// Removes and returns the back of the list when `!self.is_empty()`
+    ///
+    /// # Returns
+    ///  * `Some(old_back)` the old back of the list when `!self.is_empty()`
+    ///  * `None` when `self.is_empty()`
+    /// # Complexity
+    ///  * O(n)
+    pub fn pop_back(&mut self) -> Option<Box<T>> {
+        let old_tail = self.tail;
+        if !old_tail.is_null() {
+            let mut it = self.head;
+            if it == old_tail {
+                self.head = std::ptr::null_mut();
+                self.tail = std::ptr::null_mut();
+                unsafe { (*old_tail).list_pop_next(Ordering::Relaxed) };
+                return Some(SLink::into::<T>(old_tail));
+            }
+            loop {
+                let next = unsafe { (*it).list_get_next(Ordering::Relaxed) };
+                if next == old_tail {
+                    unsafe {
+                        (*it).list_update_next(
+                            (*old_tail).list_pop_next(Ordering::Relaxed),
+                            Ordering::Relaxed,
+                        )
+                    };
+                    return Some(SLink::into::<T>(old_tail));
+                }
+                it = next;
+            }
+        }
+        None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::ops::Deref;
+
+    use super::*;
+
+    struct IntNode {
+        pub val: i32,
+        link: SLink,
+    }
+    impl IntNode {
+        fn new(val: i32) -> Self {
+            Self {
+                val,
+                link: SLink::new(),
+            }
+        }
+    }
+    impl SListNode for IntNode {
+        fn offset_of_link() -> usize {
+            core::mem::offset_of!(IntNode, link)
+        }
+    }
+
+    #[test]
+    fn test_simple() {
+        let mut a = Box::<IntNode>::new(IntNode::new(0));
+        let mut b = Box::<IntNode>::new(IntNode::new(1));
+        let mut c = Box::<IntNode>::new(IntNode::new(2));
+        let mut d = Box::<IntNode>::new(IntNode::new(3));
+
+        let mut list = SList::<IntNode>::new();
+
+        assert!(list.is_empty());
+        assert!(list.pop_front().is_none());
+        assert!(list.pop_back().is_none());
+
+        list.push_back(a);
+        assert!(!list.is_empty());
+        a = list.pop_back().unwrap();
+        assert_eq!(a.val, 0);
+        assert!(list.pop_front().is_none());
+
+        list.push_back(a);
+        assert!(!list.is_empty());
+        a = list.pop_front().unwrap();
+        assert_eq!(a.val, 0);
+        assert!(list.pop_back().is_none());
+
+        list.push_back(a);
+        assert!(!list.is_empty());
+        assert_eq!(list.front().unwrap().deref().val, 0);
+        assert_eq!(list.back().unwrap().deref().val, 0);
+        let bor = list.front();
+        drop(bor);
+        a = list.pop_front().unwrap();
+        assert_eq!(a.val, 0);
+
+        /*let mut a = IoReq::default();
+        let mut b = IoReq::default();
+        let mut c = IoReq::default();
+        let mut d = IoReq::default();
+        let pa = unsafe { NonNull::new_unchecked(&mut a as *mut IoReq) };
+        let pb = unsafe { NonNull::new_unchecked(&mut b as *mut IoReq) };
+        let pc = unsafe { NonNull::new_unchecked(&mut c as *mut IoReq) };
+        let _pd = unsafe { NonNull::new_unchecked(&mut d as *mut IoReq) };
+
+        let mut ready0 = IoReqFifo::new();
+        assert!(unsafe { ready0.pop_front() }.is_none());
+        assert!(ready0.is_empty());
+
+        unsafe { ready0.push_back(pa) };
+        assert!(!ready0.is_empty());
+
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pa);
+        assert!(ready0.is_empty());
+
+        unsafe { ready0.push_back(pa) };
+        assert!(!ready0.is_empty());
+        unsafe { ready0.push_back(pb) };
+        assert!(!ready0.is_empty());
+        unsafe { ready0.push_back(pc) };
+        assert!(!ready0.is_empty());
+
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pa);
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pb);
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pc);
+        assert!(ready0.is_empty());
+
+        let mut ready1 = IoReqFifo::new();
+        unsafe { ready0.push_back(pa) };
+        assert!(!ready0.is_empty());
+        unsafe { ready0.push_back(pb) };
+        unsafe { ready1.push_back(pc) };
+        assert!(!ready1.is_empty());
+
+        ready0.push_back_all(&mut ready1);
+        assert!(!ready0.is_empty());
+        assert!(ready1.is_empty());
+
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pa);
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pb);
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pc);
+        assert!(ready0.is_empty());
+
+        unsafe { ready1.push_back(pa) };
+        // println!(" * * * ready1: {ready1:?}");
+        unsafe { ready1.push_back(pb) };
+        // println!(" * * * ready1: {ready1:?}");
+        unsafe { ready1.push_back(pc) };
+        // println!(" * * * ready1: {ready1:?}");
+        assert!(!ready1.is_empty());
+        ready0.push_back_all(&mut ready1);
+        assert!(!ready0.is_empty());
+        assert!(ready1.is_empty());
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pa);
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pb);
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pc);
+        assert!(ready0.is_empty());
+
+        unsafe { ready0.push_back(pa) };
+        assert!(!ready0.is_empty());
+        ready0.push_back_all(&mut ready1);
+        assert_eq!(unsafe { ready0.pop_front() }.unwrap(), pa);
+        assert!(ready0.is_empty());*/
+    }
 }
--- xaio-core/src/collection/snode.rs
+++ xaio-core/src/collection/snode.rs
@@ -1,56 +1,72 @@
-use std::{pin::Pin, sync::atomic::Ordering};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use super::NodeRef;
 
 pub trait SListNode: Sized {
     fn offset_of_link() -> usize;
-    // type Thiz;
-    // unsafe fn as_intrusive<'a>(&'a self) -> &'a SListIntrusive;
-    // unsafe fn as_mut_intrusive<'a>(&'a mut self) -> &'a mut SListIntrusive;
-    // unsafe fn from_instrusive<'a>(i: &'a SListIntrusive) -> &'a Self::Thiz;
-    // unsafe fn from_mut_instrusive<'a>(i: &'a mut SListIntrusive) -> &'a mut Self::Thiz;
 }
 
-pub struct SListLink {
-    next: std::sync::atomic::AtomicUsize,
+pub struct SLink {
+    next: AtomicUsize,
 }
 
-impl SListLink {
+impl SLink {
+    pub const fn new() -> SLink {
+        Self {
+            next: AtomicUsize::new(0),
+        }
+    }
+
     const IN_A_LIST_BIT: usize = 1usize;
 
     #[inline(always)]
-    pub(crate) fn from<T: SListNode>(mut node: Box<T>) -> *mut SListLink {
+    pub(crate) fn from<T: SListNode>(mut node: Box<T>) -> *mut SLink {
         let uptr = node.as_mut() as *mut T as usize + T::offset_of_link();
         std::mem::forget(node);
         uptr as _
     }
     #[inline(always)]
-    pub(crate) fn into<T: SListNode>(link: *mut SListLink) -> Box<T> {
+    pub(crate) fn into<T: SListNode>(link: *mut SLink) -> Box<T> {
         let uptr = link as usize - T::offset_of_link();
         unsafe { Box::from_raw(uptr as *mut T) }
     }
 
+    #[inline(always)]
+    pub(crate) fn into_ref<'a, Collection, T: SListNode>(
+        col: &'a Collection,
+        link: *const SLink,
+    ) -> Option<NodeRef<'a, Collection, T>> {
+        if link.is_null() {
+            None
+        } else {
+            let uptr = link as usize - T::offset_of_link();
+            Some(NodeRef::new(col, unsafe { &*(uptr as *mut T) }))
+        }
+    }
+
     #[inline]
     pub(crate) fn in_a_list(&self) -> bool {
-        (self.next.load(Ordering::Relaxed) & SListLink::IN_A_LIST_BIT) != 0
+        (self.next.load(Ordering::Relaxed) & SLink::IN_A_LIST_BIT) != 0
     }
     #[inline]
-    pub(crate) fn list_set_next(&mut self, next: *const SListLink, order: Ordering) {
+    pub(crate) fn list_set_next(&mut self, next: *const SLink, order: Ordering) {
         debug_assert!(!self.in_a_list());
         self.next
-            .store((next as usize) | SListLink::IN_A_LIST_BIT, order);
+            .store((next as usize) | SLink::IN_A_LIST_BIT, order);
     }
     #[inline]
-    pub(crate) fn list_update_next(&mut self, next: *const SListLink, order: Ordering) {
+    pub(crate) fn list_update_next(&mut self, next: *const SLink, order: Ordering) {
         debug_assert!(self.in_a_list());
         self.next
-            .store((next as usize) | SListLink::IN_A_LIST_BIT, order);
+            .store((next as usize) | SLink::IN_A_LIST_BIT, order);
     }
     #[inline]
-    pub(crate) fn list_get_next(&self, order: Ordering) -> *mut SListLink {
-        (self.next.load(order) & !SListLink::IN_A_LIST_BIT) as *mut SListLink
+    pub(crate) fn list_get_next(&self, order: Ordering) -> *mut SLink {
+        (self.next.load(order) & !SLink::IN_A_LIST_BIT) as *mut SLink
     }
     #[inline]
-    pub(crate) fn list_pop_next(&self, order: Ordering) -> *mut SListLink {
-        let old_next = (self.next.load(order) & !SListLink::IN_A_LIST_BIT) as *mut SListLink;
+    pub(crate) fn list_pop_next(&self, order: Ordering) -> *mut SLink {
+        let old_next = (self.next.load(order) & !SLink::IN_A_LIST_BIT) as *mut SLink;
         self.next.store(0usize, Ordering::Relaxed);
         old_next
     }
